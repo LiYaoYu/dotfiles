0e2f126cc91ee1858170e41a3c37c0d22080346a
Yao-Yu Li <yaoyuli1993@gmail.com>
2022-04-02 01:56:18 +0800

    Update nvim String/Number highlighting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 require('basic')
+require('syntax')
 require('map')
 require('bufferline')
 require('statusline')
---
+++ nvim/lua/syntax.lua
@@ -0,0 +1,2 @@
+vim.highlight.create('String', {ctermfg=81}, false)
+vim.highlight.create('Number', {ctermfg=209}, false)
