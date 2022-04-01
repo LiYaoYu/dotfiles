98b4d755e19d58c047eba966afdc12a92620cd5f
Yao-Yu Li <yaoyuli1993@gmail.com>
2022-04-02 02:13:28 +0800

    Update nvim Function highlighting

--- nvim/lua/syntax.lua
+++ nvim/lua/syntax.lua
@@ -1,2 +1,3 @@
-vim.highlight.create('String', {ctermfg=81}, false)
+vim.highlight.create('Function', {ctermfg=81}, false)
+vim.highlight.create('String', {ctermfg=108}, false)
 vim.highlight.create('Number', {ctermfg=209}, false)
