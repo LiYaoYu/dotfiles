d522c8cf3ca70d955b1f3314ce796505a0223a5d
Yao-Yu Li <yaoyuli1993@gmail.com>
2022-04-03 23:48:55 +0800

    Update nvim Comment highlighting

--- nvim/lua/syntax.lua
+++ nvim/lua/syntax.lua
@@ -1,3 +1,4 @@
 vim.highlight.create('Function', {ctermfg=81}, false)
 vim.highlight.create('String', {ctermfg=108}, false)
 vim.highlight.create('Number', {ctermfg=209}, false)
+vim.highlight.create('Comment', {ctermfg=242}, false)
