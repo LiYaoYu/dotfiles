9eab1d277d03efa43b898b6ad8ff66e656ed574b
Yao-Yu Li <yaoyuli1993@gmail.com>
2023-05-16 22:32:51 +0800

    Fix typo

--- zsh/simple.zsh
+++ zsh/simple.zsh
@@ -3,8 +3,7 @@ ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%}"
 
 function git_prompt_info() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo "%F{white}$(parse_git_dirty) ${ref#refs/heads/}%F{178} "
+  echo " $(parse_git_dirty) ${ref#refs/heads/}%F{178}"
 }
 
-# PROMPT='%{$fg[blue]%}[%1~$(git_prompt_info)%{$fg[blue]%}] %{$reset_color%}'
-PROMPT='%B%F{178}➜ %n@%m $(git_prompt_info)%{$reset_color%}'
+PROMPT='%B%F{178}➜ %n@%m %F{white}(%F{75}%1~$(git_prompt_info)%F{white})%{$reset_color%} '
