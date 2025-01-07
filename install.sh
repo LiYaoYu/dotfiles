ede2d05681cd3d701d1e181e0f506189b50b7fe1
Yao-Yu Li <yaoyuli1993@gmail.com>
2025-01-07 14:23:11 +0000

    Fix installation script

--- install.sh
+++ install.sh
@@ -1,31 +1,30 @@
 #!/usr/bin/env bash
 
+
 show_help() {
     echo "Usage: install.sh [OPTION] ..."
     echo "This script setup the environment for daily usage, there are several"
     echo "options can be configured as below:"
     echo "  -h    show this help description"
-    echo "  -m    setup the mosh server"
     exit 0
 }
+
 
 get_cli_arg() {
     . /etc/os-release
 
     DISTRIBUTION=$ID
-    MOSH_SUPPORT=false
 
-    for i in "$@"; do
+    for _ in "$@"; do
         case $1 in
             -h) show_help ;;
-            -m) MOSH_SUPPORT=true ;;
         esac
         shift
     done
 
     echo "DISTRIBUTION   = ${DISTRIBUTION}"
-    echo "MOSH_SUPPORT   = ${MOSH_SUPPORT}"
 }
+
 
 init_variables() {
     if [ "$DISTRIBUTION" = "elementary" ] || [ "$DISTRIBUTION" = "ubuntu" ]; then
@@ -40,12 +39,13 @@ init_variables() {
     fi
 }
 
+
 install_requirements_from_package_management_system() {
-    REQUIREMENTS="tmux zsh ripgrep"
+    REQUIREMENTS="tmux zsh ripgrep fzf mosh neovim"
 
     if [ "$DISTRIBUTION" = "elementary" ] || [ "$DISTRIBUTION" = "ubuntu" ]; then
         PYTHON="python3-dev python3-pip"
-        REQUIREMENTS="${REQUIREMENTS} build-essential csvtool silversearcher-ag"
+        REQUIREMENTS="${REQUIREMENTS} build-essential silversearcher-ag"
     else # including arch & manjaro
         PYTHON="python python-pip"
         REQUIREMENTS="${REQUIREMENTS} base-devel the_silver_searcher"
@@ -53,63 +53,51 @@ install_requirements_from_package_management_system() {
 
     REQUIREMENTS="${REQUIREMENTS} ${PYTHON}"
 
-    if [ "$MOSH_SUPPORT" = "true" ]; then
-        REQUIREMENTS="${REQUIREMENTS} mosh"
-    fi
-
-    sudo $PKG_DB_UPDATE
-    sudo $PKG_INSTALL $REQUIREMENTS
+    sudo "$PKG_DB_UPDATE"
+    sudo "$PKG_INSTALL $REQUIREMENTS"
 }
 
-install_requirements_from_non_package_management_system() {
-    # fzf installation
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
 
+install_requirements_from_non_package_management_system() {
     if [ "$DISTRIBUTION" = "arch" ] || [ "$DISTRIBUTION" = "manjaro" ]; then
         # yay installation
-	mkdir /tmp/yay-install
+        mkdir /tmp/yay-install
         git clone https://aur.archlinux.org/yay.git /tmp/yay-install/yay
         (cd /tmp/yay-install/yay && makepkg -si)
         rm -rf /tmp/yay-install
-
-        # tabview installation
-        yay -S tabview
     fi
 }
 
 
 install_and_set_zsh() {
-    # Install zplug
     curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
-    chsh -s `which zsh`
+    while [ ! -f ~/.zplug/init.zsh ] ; do
+        sleep 1
+    done
 
-    # TODO
-    # 1. install zplug plugins and update the .zsh theme
-    # 2. make directory ~/.zplug/repos/woefe/git-prompt.zsh/simple
-    # 3. soft link the simple.zsh in ~/.zplug/repos/woefe/git-prompt.zsh/simple
+    zsh zsh/zplug_install.zsh
+    ln -fs "$(pwd)"/zsh/zshrc ~/.zshrc
+    ln -fs "$(pwd)"/zsh/simple.zsh ~/.zplug/repos/woefe/git-prompt.zsh/simple.zsh
+
+    chsh -s "$(which zsh)"
 }
 
 
 install_and_set_tmux() {
-    ln -fs `pwd`/tmux/tmux.conf ~/.tmux.conf
+    ln -fs "$(pwd)"/tmux/tmux.conf ~/.tmux.conf
 }
 
 
 install_and_set_git() {
-    cp `pwd`/git/gitconfig ~/.gitconfig
-    ln -fs `pwd`/git/gitmessage.txt ~/.gitmessage.txt
+    cp "$(pwd)"/git/gitconfig ~/.gitconfig
+    ln -fs "$(pwd)"/git/gitmessage.txt ~/.gitmessage.txt
 }
 
 
 install_and_set_neovim() {
     mkdir -p ~/.config
-
-    ln -fs `pwd`/nvim ~/.config/nvim
-
-    # TODO: install neovim packages
-    git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    ln -fs "$(pwd)"/nvim ~/.config/nvim
 }
 
 
---
+++ zsh/zplug_install.zsh
@@ -0,0 +1,7 @@
+source ~/.zplug/init.zsh
+
+zplug 'Aloxaf/fzf-tab'
+zplug 'woefe/git-prompt.zsh', use:'{git-prompt.zsh,simple.zsh}'
+zplug 'tom-auger/cmdtime'
+zplug 'zsh-users/zsh-autosuggestions'
+zplug install
