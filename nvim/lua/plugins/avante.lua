b43134e543758d6915f5780634f9218755556278
Yao-Yu Li <yaoyuli1993@gmail.com>
2025-03-15 17:35:24 +0800

    Insert avante suggestion with cursor planning mode

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -1,16 +1,33 @@
+-- Set ANTHROPIC_API_KEY to empty here to avoid avante chat popup for
+-- ANTHROPIC_API_KEY in some version
+vim.env.ANTHROPIC_API_KEY = ''
+
 return {
-    'yetone/avante.nvim', tag = 'v0.0.21',
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
     version = false,
     opts = {
-        provider = "openai",
+        provider = 'openai',
         openai = {
-            endpoint = "https://api.openai.com/v1",
-            model = "o3-mini",
-            api_key_name = 'cmd: cat ' .. os.getenv('HOME') .. '/.openai/api.key'
+            endpoint = 'https://api.openai.com/v1',
+            api_key_name = 'cmd: cat ' .. os.getenv('HOME') .. '/.openai/api.key',
         },
         mappings = {
             ask = '<A-l>',
-            edit = '<A-;>'
+            edit = '<A-;>',
+        },
+        cursor_applying_provider = "groq",
+        behaviour = {
+            enable_cursor_planning_mode = true,
+        },
+        vendors = {
+            groq = {  -- Configuration for groq provider
+                __inherited_from = "openai",
+                api_key_name = 'cmd: cat ' .. os.getenv('HOME') .. '/.groq/api.key',
+                endpoint = "https://api.groq.com/openai/v1/",
+                model = "llama-3.3-70b-versatile",
+                max_tokens = 32768,  -- Ensure sufficient tokens for processing
+            },
         },
     },
     dependencies = {
@@ -19,10 +36,6 @@ return {
         'nvim-lua/plenary.nvim',
         'MunifTanjim/nui.nvim',
         'nvim-tree/nvim-web-devicons',
-        {
-            'HakonHarnes/img-clip.nvim',
-            event = 'VeryLazy',
-        },
         {
             'MeanderingProgrammer/render-markdown.nvim',
             opts = {
