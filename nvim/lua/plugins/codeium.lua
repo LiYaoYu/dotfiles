99e7dc71fb7cbafdc3f329d926f449eb6e44768b
Yao-Yu Li <yaoyuli1993@gmail.com>
2025-04-20 00:26:08 +0800

    Fix the codeium slow down the whole nvim issue

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -2,6 +2,11 @@ return {
     'Exafunction/codeium.vim',
     config = function ()
         vim.g.codeium_disable_bindings = 1
-        vim.keymap.set('i', '<A-k>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+        vim.g.codeium_idle_delay = 250
+
+        vim.keymap.set(
+            'i', '<A-k>',
+            function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true }
+        )
     end
 }
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,11 +1,23 @@
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = {
-        'nvim-tree/nvim-web-devicons'
-    },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+        local last_status = nil
+
         local function codeiumStatus()
-            return 'Codeium:' .. vim.api.nvim_call_function('codeium#GetStatusString', {})
+            -- Use vim.schedule to make the status check asynchronous
+            vim.schedule(function()
+                local ok, status = pcall(vim.fn['codeium#GetStatusString'])
+
+                if ok and status ~= last_status then
+                    last_status = status
+                    vim.g.codeium_status = status
+
+                    require('lualine').refresh()
+                end
+            end)
+
+            return 'Codeium: ' .. (last_status or 'Loading...')
         end
 
         require('lualine').setup {
